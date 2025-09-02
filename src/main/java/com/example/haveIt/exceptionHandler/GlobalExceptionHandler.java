4eba47d4a09f061c248c37b5c9a9db9533c658b1
Jaguar-188 <ruturajlokhande9@gmail.com>
2025-09-02 19:43:09 +0530

    global exception handler and exception classes

--- src/main/java/com/example/haveIt/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/haveIt/exceptionHandler/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package com.example.haveIt.exceptionHandler;
 
-import com.example.haveIt.entity.exception.CustomerNotFoundException;
-import com.example.haveIt.entity.exception.EmailAlreadyExistsException;
-import com.example.haveIt.entity.exception.ExceptionMessage;
+import com.example.haveIt.entity.exception.*;
 import com.example.haveIt.utils.Logging;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +19,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ExceptionMessage> customerNotFoundExceptionHandler(CustomerNotFoundException customerNotFoundException,
                                                                                 WebRequest webRequest){
 
-        log.info("Resolving customer not found exception");
+        log.error("Resolving customer not found exception");
         ExceptionMessage exceptionMessage = new ExceptionMessage(
                 customerNotFoundException.getMessage(),
                 webRequest.getDescription(false).replace("uri="," "),
@@ -37,7 +35,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<ExceptionMessage> emailAlreadyExistsExceptionHandler(EmailAlreadyExistsException emailAlreadyExistsException,
                                                                                   WebRequest webRequest){
 
-        log.info("Resolving customer not found exception");
+        log.error("Resolving customer not found exception");
         ExceptionMessage exceptionMessage = new ExceptionMessage(
                 emailAlreadyExistsException.getMessage(),
                 webRequest.getDescription(false).replace("uri="," "),
@@ -47,5 +45,37 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         );
 
         return new ResponseEntity<>(exceptionMessage,HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @ExceptionHandler(ItemNotFoundException.class)
+    protected ResponseEntity<ExceptionMessage> ItemNotFoundExceptionHandler(ItemNotFoundException itemNotFoundException,
+                                                                                WebRequest webRequest){
+
+        log.error("Resolving item not found exception");
+        ExceptionMessage exceptionMessage = new ExceptionMessage(
+                itemNotFoundException.getMessage(),
+                webRequest.getDescription(false).replace("uri="," "),
+                String.valueOf(HttpStatus.NOT_FOUND),
+                String.valueOf(HttpStatus.NOT_FOUND).substring(0,3),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(exceptionMessage,HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    protected ResponseEntity<ExceptionMessage> OrderNotFoundExceptionHandler(OrderNotFoundException orderNotFoundException,
+                                                                            WebRequest webRequest){
+
+        log.error("Resolving order not found exception");
+        ExceptionMessage exceptionMessage = new ExceptionMessage(
+                orderNotFoundException.getMessage(),
+                webRequest.getDescription(false).replace("uri="," "),
+                String.valueOf(HttpStatus.NOT_FOUND),
+                String.valueOf(HttpStatus.NOT_FOUND).substring(0,3),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(exceptionMessage,HttpStatus.NOT_FOUND);
     }
 }
